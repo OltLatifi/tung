d9be795ab894a88f6c3e2cd2967ac8c61dec2df2
OltLatifi <oltlatifi2003@gmail.com>
2023-07-19 19:49:34 +0200

    Exclude values on user_profile

--- auth_api/helpers.py
+++ auth_api/helpers.py
@@ -2,6 +2,12 @@ from .models import User
 from django.forms import model_to_dict
 
 def get_full_user(data):
+    excluded_values = ["password", "last_login",
+                       "is_superuser", "first_name",
+                       "last_name", "is_staff",
+                       "is_active", "date_joined",
+                       "permissions", "groups",
+                       "user_permissions"]
     user = User.objects.get(id=data.get("user"))
-    data["user"] = model_to_dict(user)
+    data["user"] = model_to_dict(user, exclude=excluded_values)
     return data
