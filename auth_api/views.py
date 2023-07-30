68ba353e5746dbce27d23882581e642defe3b1bb
OltLatifi <44296923+OltLatifi@users.noreply.github.com>
2023-07-30 15:32:13 +0200

    [TUNG-API] - Endpoint to update user profile (#6)

--- auth_api/views.py
+++ auth_api/views.py
@@ -1,9 +1,11 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from .serializers import user_register_serializer, create_user_profile
+from .serializers import user_register_serializer, user_profile_serializer
 from rest_framework.response import Response
 from rest_framework import status
 from .helpers import get_full_user
+from .models import Profile
+from django.shortcuts import get_object_or_404
 
 @api_view(["POST"])
 def user_register(request):
@@ -16,16 +18,21 @@ def user_register(request):
     else:
         return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(["POST"])
+@api_view(["POST", "PATCH"])
 @permission_classes([IsAuthenticated])
 def user_profile(request):
-    serializer_class = create_user_profile
+    serializer_class = user_profile_serializer
+    request.data["user"] = request.user.id
 
-    serializer = serializer_class(data=request.data)
+    if request.method == "POST":
+        serializer = serializer_class(data=request.data)
+    elif request.method == "PATCH":
+        profile = get_object_or_404(Profile, user=request.data["user"])        
+        serializer = serializer_class(profile, data=request.data, partial=True)
+
     if serializer.is_valid():
         serializer.save()
         profile = get_full_user(serializer.data)
         return Response({"user": profile}, status=status.HTTP_201_CREATED)
     else:
         return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-    # return Response({"a":request.user})
